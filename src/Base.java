0b2c6d8ce3afe94e2d3978ddd3853aae1703d150
Mathis <mathisdesnoes@gmail.com>
2020-03-20 10:48:57 +0100

    algo glouton

--- src/Base.java
+++ src/Base.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class Base {
-	int cout;
-	int nbElements;
+	
+	private int cout;
+	private int nbElements;
 	private ArrayList<String> entreprises;
 	
 	
 	public Base(int cout, int nbElements, ArrayList<String> entreprises) {
-		super();
 		this.cout = cout;
 		this.nbElements = nbElements;
 		this.entreprises = entreprises;
@@ -17,28 +17,18 @@ public class Base {
 	public int getCout() {
 		return cout;
 	}
-
-
 	public void setCout(int cout) {
 		this.cout = cout;
 	}
-
-
 	public int getNbElements() {
 		return nbElements;
 	}
-
-
 	public void setNbElements(int nbElements) {
 		this.nbElements = nbElements;
 	}
-
-
 	public ArrayList<String> getEntreprises() {
 		return entreprises;
 	}
-
-
 	public void setEntreprises(ArrayList<String> entreprises) {
 		this.entreprises = entreprises;
 	}
--- src/main.java
+++ src/main.java
@@ -5,12 +5,19 @@ public class main {
 
 	public static void main(String[] args) throws IOException{
 
-		FichierUtils fichEntreprises = new FichierUtils("data/scenarios/liste_entreprises/Liste Ent1.txt");
+		FichierUtils fichEntreprises = new FichierUtils("data/scenarios/liste_entreprises/Liste Ent3.txt");
 		
 		List<String> entreprises = fichEntreprises.getLignes();
+		entreprises.remove(0); // On supprime la première ligne qui correspond au nombre d'entreprises
 		entreprises.forEach(entreprise ->
 			System.out.println(entreprise)
 		);
+		
+		ListeBase listeBase = new ListeBase("data/scenarios/liste_bases/Liste Bases3.txt");
+		Glouton glouton = new Glouton(entreprises, listeBase);
+		
+		List<Base> basesOpti = glouton.execute();
+		
 		
 		
 		//BRANCH & BOUND
